c009a005958908bb35f39500dffdc87b96ec4afc
waperdomob <wilmerp30@gmail.com>
2023-06-17 16:20:27 -0500

    Se ajusta para permitir acceso frontend

--- softseguros_back/settings/base.py
+++ softseguros_back/settings/base.py
@@ -105,12 +105,14 @@ USE_I18N = True
 USE_TZ = True
 
 CORS_ALLOWED_ORIGINS = [
+    "https://softseguros-front.vercel.app",
     "http://localhost:8080",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
 CORS_ORIGIN_WHITELIST = [
+    "https://softseguros-front.vercel.app",
     "http://localhost:8080",
     "http://127.0.0.1:8000",
     "http://localhost:3000",
